77a73c41a1ca6728faa3344ed30a543df909afd1
Anmol376OP <anmolsrivastav7102@gmail.com>
2023-01-28 12:38:26 +0530

    Navbar Responsive

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,18 @@
 import React, { useState } from "react";
 import { Link } from 'react-router-dom';
 import './Navbar.css';
+import menuImg from '../assets/menu.png';
+import closeImg from '../assets/close.png';
 
 export default function Navbar() {
     const [isActive, setActive] = useState(false)
     return (
         <div className="Container">
             <h2>Anmol</h2>
-            <div className={isActive ? "toggleactive toggle" : "toggle"} onClick={() => { setActive(!isActive) }}></div>
+            <div className={isActive ? "toggleactive toggle" : "toggle"} onClick={() => { setActive(!isActive) }}>
+                <img className={!isActive ? "i1" : "disappear"} src={menuImg}></img>
+                <img className={!isActive ? "disappear" : "i1"} src={closeImg}></img>
+            </div>
             <ul className={isActive ? "nav active" : "nav inactive"} onClick={() => { setActive(!isActive) }}>
                 <li><Link to='/' className="elem">Home</Link></li>
                 <li><Link to='/about' className="elem">About</Link></li>
