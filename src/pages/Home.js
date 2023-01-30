f90a3ed684dee657e79761708630a9039909e208
Anmol376OP <anmolsrivastav7102@gmail.com>
2023-01-30 18:40:25 +0530

    Made homepage responsive

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,53 +2,76 @@ import React from 'react'
 import '../styles/Home.css'
 import Navbar from '../components/Navbar'
 import { Parallax, ParallaxLayer } from '@react-spring/parallax'
-
+import Anmol from '../assets/anmol.png'
 export default function Home() {
     return (
         <div>
-            <Parallax pages={2} style={{ top: '0', left: '0' }} class="animation">
+            <Parallax pages={2} style={{ top: '0', left: '0' }} className="animation">
                 <ParallaxLayer offset={0} style={{ zIndex: "20" }}>
                     <Navbar />
                 </ParallaxLayer>
                 <ParallaxLayer offset={0} speed={0.25}>
-                    <div class="animation_layer parallax" id="artback"></div>
+                    <div className="animation_layer parallax" id="artback"></div>
                 </ParallaxLayer>
                 <ParallaxLayer offset={0} speed={0.3}>
-                    <div class="animation_layer parallax" id="mountain"></div>
+                    <div className="animation_layer parallax" id="mountain"></div>
                 </ParallaxLayer>
                 <ParallaxLayer offset={0} speed={-0.1}>
-                    <div class="animation_layer parallax" id="Text"></div>
+                    <div className="animation_layer parallax" id="Text"></div>
                 </ParallaxLayer>
                 <ParallaxLayer offset={0} speed={0.3}>
-                    <div class="animation_layer parallax" id="jungle1"></div>
+                    <div className="animation_layer parallax" id="jungle1"></div>
                 </ParallaxLayer>
                 <ParallaxLayer offset={0} speed={0.35}>
-                    <div class="animation_layer parallax" id="jungle2"></div>
+                    <div className="animation_layer parallax" id="jungle2"></div>
                 </ParallaxLayer>
                 <ParallaxLayer offset={0} speed={0.5}>
-                    <div class="animation_layer parallax" id="jungle3"></div>
+                    <div className="animation_layer parallax" id="jungle3"></div>
                 </ParallaxLayer>
                 <ParallaxLayer offset={0} speed={0.45}>
-                    <div class="animation_layer parallax" id="jungle4"></div>
+                    <div className="animation_layer parallax" id="jungle4"></div>
                 </ParallaxLayer>
                 <ParallaxLayer offset={0} speed={0.40}>
-                    <div class="animation_layer parallax" id="manonmountain"></div>
+                    <div className="animation_layer parallax" id="manonmountain"></div>
                 </ParallaxLayer>
                 <ParallaxLayer offset={0} speed={0.35}>
-                    <div class="animation_layer parallax" id="jungle5"></div>
+                    <div className="animation_layer parallax" id="jungle5"></div>
                 </ParallaxLayer>
                 <ParallaxLayer offset={1} speed={0.25}>
-                    <section className='sec'>
-                        <div className='ContainerBox'>
-                            <p className='uptxt'>Hi</p>
-                            <div className='splitText'>
-                                <p className='Im'>I am</p>
-                                <p className='myname'>Anmol Srivastava</p>
+                    <div className='sec'>
+                        <div className='Alpha'>
+                            <div className='Beta1'>
+                                <p className='hi'>Hi,</p>
+                                <div className='SplitText'>
+                                    <p className='Im'>I am</p>
+                                    <p className='Name'>Anmol Srivastava</p>
+                                </div>
+                                <p className='Dev'>I'm a Web Developer</p>
+                            </div>
+                            <div className='Beta2'>
+                                <img src={Anmol} />
+                            </div>
+                        </div>
+                    </div>
+
+                </ParallaxLayer>
+                <ParallaxLayer offset={1} speed={0.25} className="xyzzz">
+                    <div className='sec2'>
+                        <div className='Alpha2'>
+                            <div className='Beta22'>
+                                <img src={Anmol} />
+                            </div>
+                            <div className='Beta12'>
+                                <p className='hi2'>Hi,</p>
+                                <div className='SplitText2'>
+                                    <p className='Im2'>I am</p>
+                                    <p className='Name2'>Anmol Srivastava</p>
+                                </div>
+                                <p className='Dev2'>I'm a Web Developer</p>
                             </div>
 
-                            <p className='dev'>I am a Web Developer</p>
                         </div>
-                    </section>
+                    </div>
                 </ParallaxLayer>
             </Parallax>
         </div>
